d749e925c45811245c93c6f137a297226f2466a4
Eduardo Gutierrez <edd_d@mit.edu>
2012-02-28 14:07:41 -0500

    We're redoing step definitions and removing training wheels

--- features/step_definitions/user_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^I am a worker$/ do
-  @user = Factory(:besk_worker)
-end
-
-Given /^I am a user who has received packages$/ do
-  @user = Factory(:user)
-  @packages = @user.received_packages
-end
