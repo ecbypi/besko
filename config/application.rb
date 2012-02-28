6fc541a66d255f6b0edd16511755cf2375daf99c
Eduardo Gutierrez <edd_d@mit.edu>
2012-02-28 14:45:12 -0500

    Add default URL options for links in emails

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Besko
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Default url information
+    config.action_mailer.default_url_options = { host: 'besko.mit.edu', protocol: 'https' }
   end
 end
