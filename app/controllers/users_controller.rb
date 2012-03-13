fa5846faa8b3ffd7256dc748cdaaeaa424e6b1bc
Eduardo Gutierrez <edd_d@mit.edu>
2012-03-13 02:59:57 -0400

    Add UsersControlelr#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     users = User.recipients(params[:term] || "", use_ldap: params[:remote])
     respond_with(users)
   end
+
+  def create
+    respond_with User.create_with_or_without_password(params[:user])
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe UsersController do
   include Devise::TestHelpers
 
+  def json_body
+    @json_body ||= JSON.parse(response.body)
+  end
+
   describe "GET index.json" do
     let(:user) { FactoryGirl.create(:user) }
     let!(:result) { FactoryGirl.create(:mrhalp) }
     let(:role) { FactoryGirl.create(:role, title: 'Besk Worker') }
-
-    def json_body
-      @json_body ||= JSON.parse(response.body)
-    end
 
     before :each do
       user.roles << role
@@ -31,6 +31,28 @@ describe UsersController do
       user.should have_key 'login'
       user.should have_key 'email'
       user.should have_key 'id'
+    end
+  end
+
+  describe "POST create.json" do
+    let(:attributes) do
+      {
+        first_name: 'Doctor',
+        last_name: 'Halp',
+        login: 'drhalp',
+        email: 'drhalp@mit.edu'
+      }
+    end
+
+    let(:user) { FactoryGirl.create(:user, attributes) }
+
+    before :each do
+      User.stub(:create_with_or_without_password).and_return(user)
+      post :create, format: :json, user: attributes
+    end
+
+    it "returns the persisted user information" do
+      json_body.should have_key 'id'
     end
   end
 end
