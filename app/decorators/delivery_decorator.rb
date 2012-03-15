8464484823e81f055614af8bbace103b0b2a36a5
Eduardo Gutierrez <edd_d@mit.edu>
2012-03-15 10:57:46 -0400

    Add custom #as_json method for DeliveryDecorator

--- app/decorators/delivery_decorator.rb
+++ app/decorators/delivery_decorator.rb
@@ -1,3 +1,13 @@
 class DeliveryDecorator < ApplicationDecorator
   decorates :delivery
+
+  def as_json options={}
+    model.as_json(
+      include: [:worker],
+      except: [:updated_at, :delivered_on],
+      methods: [:package_count]
+    ).merge(
+      delivered_at: model.created_at.strftime('%r')
+    ).stringify_keys
+  end
 end
--- spec/decorators/delivery_decorator_spec.rb
+++ spec/decorators/delivery_decorator_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe DeliveryDecorator do
-  before { ApplicationController.new.set_current_view_context }
+
+  let(:worker) { FactoryGirl.create(:user) }
+  let(:delivery) { FactoryGirl.create(:delivery, deliverer: 'UPS', created_at: '2010-10-30 10:30:10', worker: worker) }
+  let(:decorator) { DeliveryDecorator.new(delivery) }
+
+  describe "#as_json" do
+    let(:json) { decorator.as_json }
+    it "forwards model.deliverer" do
+      json.should have_key 'deliverer'
+      json['deliverer'].should eq 'UPS'
+    end
+
+    it "includes worker attributes" do
+      json.should have_key 'worker'
+      json['worker'].should have_key 'id'
+      json['worker'].should have_key 'first_name'
+      json['worker'].should have_key 'last_name'
+      json['worker'].should have_key 'login'
+      json['worker'].should have_key 'email'
+    end
+
+    it "forwards package count" do
+      json.should have_key 'package_count'
+    end
+
+    it "forwards formatted created_at" do
+      json.should have_key 'delivered_at'
+    end
+  end
 end
