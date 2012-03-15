7644b68e42d1a8e7c964e1341c5f9c2dc7a91ca2
Eduardo Gutierrez <edd_d@mit.edu>
2012-03-15 05:48:25 -0400

    Add dom helpers for data-resource elements

--- features/support/dom_element_helpers.rb
+++ features/support/dom_element_helpers.rb
@@ -4,14 +4,26 @@ module DOMElementHelpers
     find :model_collection, name.pluralize
   end
 
+  def model_element name, text=nil
+    if text
+      find :model_element, name, text: text
+    else
+      find :model_element, name
+    end
+  end
+
   def model_resource text
     find '[data-resource-id]', text: text
   end
 
-  ['receipt', 'delivery', 'recipient'].each do |model_name|
+  ['receipt', 'delivery', 'recipient', 'user'].each do |model_name|
     class_eval <<-METHODS, __FILE__, __LINE__ + 1
       def #{model_name.pluralize}_collection
         model_collection '#{model_name.pluralize}'
+      end
+
+      def #{model_name}_element text=nil
+        model_element '#{model_name}', text
       end
     METHODS
   end
--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -1,3 +1,7 @@
 Capybara.add_selector(:model_collection) do
   xpath { |name| XPath.css("[data-collection='#{name}']") }
 end
+
+Capybara.add_selector(:model_element) do
+  xpath { |name| XPath.css("[data-resource='#{name}']") }
+end
