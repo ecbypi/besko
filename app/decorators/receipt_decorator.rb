076bda29ef4d956686d7ff100c4d65a3ed6c842a
Eduardo Gutierrez <edd_d@mit.edu>
2012-03-20 21:49:09 -0400

    Make sign out button not remote: true

--- app/decorators/receipt_decorator.rb
+++ app/decorators/receipt_decorator.rb
@@ -9,7 +9,7 @@ class ReceiptDecorator < ApplicationDecorator
     if model.signed_out?
       model.signed_out_at.strftime(time_format)
     else
-      h.button_to 'Sign Out', h.receipt_path(model), method: :put, remote: true
+      h.button_to 'Sign Out', h.receipt_path(model), method: :put, class: 'button'
     end
   end
 end
