8b0dad28c65dbf681c9d99deee696ab114072860
Eduardo Gutierrez <edd_d@mit.edu>
2012-04-05 01:18:38 -0400

    Remove test on title uniqueness for roles

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Role do
   let!(:role) { create(:role) }
 
-  it { should validate_uniqueness_of(:title).case_insensitive }
   it { should have_many(:user_roles) }
   it { should have_many(:users).through(:user_roles) }
 end
