55d00babdb8539393b0316cf915cf0fcbf1f2683
Eduardo Gutierrez <edd_d@mit.edu>
2012-04-05 01:44:03 -0400

    Update role steps for new roles structure

--- features/step_definitions/role_steps.rb
+++ features/step_definitions/role_steps.rb
@@ -1,4 +1,4 @@
 Given /^I am a besk worker with the email "([^"]*)"$/ do |email|
   user = User.find_by_email!(email)
-  user.roles << Role.find_or_create_by_title('Besk Worker')
+  user.roles << BeskWorker.create!
 end
