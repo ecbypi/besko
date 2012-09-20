82aa40f624dcf7d1896f59f6c3bc7fe8f3edfcb9
Eduardo Gutierrez <edd_d@mit.edu>
2012-09-20 02:07:39 -0400

    Add UserRole.with_title
    Filters by role title including the associated user.

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,3 +1,7 @@
 class UserRole < ActiveRecord::Base
   guise_for :user
+
+  def self.with_title(title)
+    where(:title => title.to_s).includes(:user)
+  end
 end
--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -7,4 +7,16 @@ describe UserRole do
     create(:user_role)
     should validate_uniqueness_of(:title).scoped_to(:user_id)
   end
+
+  describe ".with_title" do
+    let!(:besk_worker) { create(:user_role) }
+    let!(:admin) { create(:user_role, title: 'Admin') }
+
+    it "selects role objects in reverse order of creation" do
+      roles = UserRole.with_title(:BeskWorker)
+
+      roles.should include besk_worker
+      roles.should_not include admin
+    end
+  end
 end
