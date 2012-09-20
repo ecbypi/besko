1c50c4c997e7fb977c84fa51b3ee7e619da5626a
Eduardo Gutierrez <edd_d@mit.edu>
2012-09-20 03:08:23 -0400

    Validate user_id presence in UserRole

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,6 +1,8 @@
 class UserRole < ActiveRecord::Base
   guise_for :user
 
+  validates :user_id, presence: true
+
   def self.with_title(title)
     where(:title => title.to_s).includes(:user)
   end
--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -7,6 +7,7 @@ describe UserRole do
     create(:user_role)
     should validate_uniqueness_of(:title).scoped_to(:user_id)
   end
+  it { should validate_presence_of(:user_id) }
 
   describe ".with_title" do
     let!(:besk_worker) { create(:user_role) }
