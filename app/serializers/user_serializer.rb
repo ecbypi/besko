cd1a2ffcbb3a1a2d9e07257bd6813cbb8bdf0508
Eduardo Gutierrez <edd_d@mit.edu>
2013-03-14 09:35:57 -0400

    Conditionally serialize User#street and User#id
    embera-data will use 'null' as a valid id so the attribute must not be
    included in order to differentiate between unpersisted records. Since
    User#street can be an empty string now, we only want to include it if it
    is present.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < ApplicationSerializer
   attributes :id, :first_name, :last_name, :email, :login, :street
+
+  def include_id?
+    object.id.present?
+  end
+
+  def include_street?
+    object.street.present?
+  end
 end
