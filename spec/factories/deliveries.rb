390541183aa30cbc605b7e34ba0660bbdf59ddf0
Eduardo Gutierrez <edd_d@mit.edu>
2013-03-19 02:42:18 -0400

    Set Delivery#delivered_on in factories

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
-
   factory :delivery do
+    ignore do
+      delivered { Time.zone.today }
+    end
+
+    delivered_on do |proxy|
+      proxy.created_at ? proxy.created_at.to_date : proxy.delivered
+    end
+
     association :worker, factory: :user
     deliverer "FedEx"
   end
