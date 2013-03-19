7ab3569399ca25a159d6975e3d9e13e9eb3ae801
Eduardo Gutierrez <edd_d@mit.edu>
2013-03-18 22:25:15 -0400

    Remove remainder of backbone cruft

--- app/assets/javascripts/besko/views.js
+++
@@ -1,5 +0,0 @@
-//= include besko/views/user_autocomplete
-//= include besko/views/delivery_search
-//= include besko/views/delivery_form
-//= include besko/views/signup_form
-//= include besko/views/roles
