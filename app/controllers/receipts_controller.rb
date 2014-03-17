29956f303df68bf7a443d6fa5ac8118074926bdc
Eduardo Gutierrez <eduardo@vermonster.com>
2014-03-17 14:39:37 -0400

    Cleanup receipts management
    * Leverage more of `inherited_resources`' hooks
    * Use `PaginatingDecorator` instead of different methods for the
      decorated collection and collection for pagination.

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,28 +1,24 @@
 class ReceiptsController < InheritedResources::Base
   respond_to :html, :js, :json
 
-  actions :all, except: [:show]
   authorize_resource
-
-  helper_method :decorated_collection
-  hide_action :decorated_collection
-
-  def update
-    resource.sign_out!
-    update!(notice: 'Package Signed Out') { receipts_path(page: params[:page]) }
-  end
 
   private
 
-  def decorated_collection
-    @decorated_receipts ||= collection.decorate
+  def smart_resource_url
+    receipts_path(page: params[:page])
+  end
+
+  def update_resource(object, *attributes)
+    object.sign_out!
   end
 
   def collection
-    get_collection_ivar || begin
-      @receipts = current_user.receipts.
-        includes(delivery: :user).
-        page(params[:page]).per(10)
+    @receipts ||= begin
+      receipts = current_user.receipts
+      receipts = receipts.includes(delivery: :user)
+      receipts = receipts.page(params[:page]).per(10)
+      PaginatingDecorator.decorate(receipts)
     end
   end
 end
