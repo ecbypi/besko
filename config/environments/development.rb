bf6f89417cc43c36782fbcb52f2b63a6ba7b215a
Eduardo Gutierrez <eduardo@vermonster.com>
2014-04-26 14:04:27 -0400

    Cache in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,9 @@ Besko::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Show full error reports and disable caching
+  # Show full error reports and enable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
