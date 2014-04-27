5dfdb84c94d388d484551e6bf888e77ba977765f
Eduardo Gutierrez <eduardo@vermonster.com>
2014-04-27 19:14:49 -0400

    Remove unused boostrap helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,6 @@
 module ApplicationHelper
   PRIORITY_COUNTRIES = %w( US CA )
 
-  def bootstrap(data)
-    content_for(:bootstrap) do
-      content_tag(:script, id: 'bootstrap', type: 'text/json') do
-        "{ \"data\" : #{ data.to_json } }"
-      end
-    end
-  end
-
-  def bootstrap_collection
-    data = ActiveModel::ArraySerializer.new(collection)
-    bootstrap(data)
-  end
-
   def body_css_class
     css_class = "#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"
 
