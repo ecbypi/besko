3c122ee8c3d85e4d163155e3f6eadc8668abda8e
Eduardo Gutierrez <eduardo@vermonster.com>
2014-04-27 17:29:33 -0400

    Run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.order = 'random'
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
