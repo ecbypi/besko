7d83d13260a1194e7d27290d0cc65a3fc32ff64f
Eduardo Gutierrez <eduardo@velvetsocks.com>
2021-02-17 15:30:10 -0500

    Update db/schema.rb for Rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -1,104 +1,98 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907000305) do
+ActiveRecord::Schema.define(version: 2014_09_07_000305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "deliveries", force: true do |t|
-    t.string   "deliverer"
-    t.integer  "user_id"
+  create_table "deliveries", force: :cascade do |t|
+    t.string "deliverer"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.date     "delivered_on"
+    t.date "delivered_on"
+    t.index ["delivered_on"], name: "index_deliveries_on_delivered_on"
+    t.index ["user_id"], name: "index_deliveries_on_worker_id"
   end
 
-  add_index "deliveries", ["delivered_on"], name: "index_deliveries_on_delivered_on", using: :btree
-  add_index "deliveries", ["user_id"], name: "index_deliveries_on_worker_id", using: :btree
-
-  create_table "forwarding_addresses", force: true do |t|
-    t.string   "street"
-    t.string   "city"
-    t.string   "state"
-    t.string   "country"
-    t.string   "postal_code"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "forwarding_addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "postal_code"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_forwarding_addresses_on_user_id"
   end
 
-  add_index "forwarding_addresses", ["user_id"], name: "index_forwarding_addresses_on_user_id", using: :btree
-
-  create_table "receipts", force: true do |t|
-    t.text     "comment"
-    t.integer  "delivery_id"
-    t.integer  "user_id"
+  create_table "receipts", force: :cascade do |t|
+    t.text "comment"
+    t.integer "delivery_id"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "number_packages"
+    t.integer "number_packages"
     t.datetime "signed_out_at"
+    t.index ["delivery_id"], name: "index_receipts_on_delivery_id"
+    t.index ["user_id"], name: "index_receipts_on_recipient_id"
   end
 
-  add_index "receipts", ["delivery_id"], name: "index_receipts_on_delivery_id", using: :btree
-  add_index "receipts", ["user_id"], name: "index_receipts_on_recipient_id", using: :btree
-
-  create_table "roles_users", id: false, force: true do |t|
+  create_table "roles_users", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
-  create_table "user_roles", force: true do |t|
-    t.integer  "user_id"
+  create_table "user_roles", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
+    t.string "title"
+    t.index ["user_id", "title"], name: "index_user_roles_on_user_id_and_title", unique: true
   end
 
-  add_index "user_roles", ["user_id", "title"], name: "index_user_roles_on_user_id_and_title", unique: true, using: :btree
-
-  create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "login"
-    t.string   "email",                              default: "",    null: false
-    t.string   "encrypted_password",     limit: 128, default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "login"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", limit: 128, default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",                    default: 0
-    t.string   "unlock_token"
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
     t.datetime "locked_at"
-    t.string   "authentication_token"
+    t.string "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "street"
-    t.boolean  "forwarding_account",                 default: false
+    t.string "street"
+    t.boolean "forwarding_account", default: false
     t.datetime "activated_at"
+    t.index ["activated_at"], name: "index_users_on_activated_at"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
-
-  add_index "users", ["activated_at"], name: "index_users_on_activated_at", using: :btree
-  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
